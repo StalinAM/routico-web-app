314391c49aac8aef4911a6d797832eb34597fc10
Stalin Acurio <sta_vla411@hotmail.com>
2024-12-24 17:16:12 -0500

    add custom colors

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      colors: {
+        azur: {
+          50: '#ffffff',
+          200: '#dcd9ff',
+          400: '#b8b3ff',
+          600: '#6941C6',
+          800: '#3e388b'
+        }
+      }
+    }
+  },
+  plugins: []
 }
