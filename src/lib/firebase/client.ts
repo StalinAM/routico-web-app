269327461634392253410401c6a7c81dc8dd3dc3
Stalin Acurio <sta_vla411@hotmail.com>
2025-01-07 01:35:01 -0500

    add credentials client and server

---
+++ src/lib/firebase/client.ts
@@ -0,0 +1,12 @@
+import { initializeApp } from 'firebase/app'
+
+const firebaseConfig = {
+  apiKey: import.meta.env.PUBLIC_APIKEY,
+  authDomain: import.meta.env.PUBLIC_AUTHDOMAIN,
+  projectId: import.meta.env.PUBLIC_PROJECTID,
+  storageBucket: import.meta.env.PUBLIC_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.PUBLIC_MESSAGINGSENDERID,
+  appId: import.meta.env.PUBLIC_APPIDs
+}
+
+export const app = initializeApp(firebaseConfig)
