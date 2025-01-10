8700fa562de61bd195a367acd5f6d3f966de6e7a
Stalin Acurio <sta_vla411@hotmail.com>
2025-01-10 10:51:46 -0500

    refactor: rename signIn function to iniciar and update event listener syntax

--- src/utils/services.js
+++ src/utils/services.js
@@ -1,38 +1,43 @@
 import {
   getAuth,
   inMemoryPersistence,
-  signInWithEmailAndPassword,
-} from "firebase/auth";
-import { app } from "../lib/firebase/client";
+  signInWithEmailAndPassword
+} from 'firebase/auth'
+import { app } from '../lib/firebase/client'
 
-const auth = getAuth(app);
+const auth = getAuth(app)
 // This will prevent the browser from storing session data
-auth.setPersistence(inMemoryPersistence);
+auth.setPersistence(inMemoryPersistence)
 
-const form = document.querySelector("form");
-export const signIn = form.addEventListener("submit", async (e) => {
-  e.preventDefault();
-  const formData = new FormData(form);
-  const email = formData.get("email")?.toString();
-  const password = formData.get("password")?.toString();
+export const iniciar = () => {
+  const form = document.querySelector('form')
+  form.addEventListener('submit', async (e) => {
+    e.preventDefault()
+    const formData = new FormData(form)
+    const email = formData.get('email')?.toString()
+    const password = formData.get('password')?.toString()
 
-  if (!email || !password) {
-    return;
-  }
-  const userCredential = await signInWithEmailAndPassword(
-    auth,
-    email,
-    password,
-  );
-  const idToken = await userCredential.user.getIdToken();
-  const response = await fetch("/api/auth/login", {
-    method: "GET",
-    headers: {
-      Authorization: `Bearer ${idToken}`,
-    },
-  });
+    if (!email || !password) {
+      return
+    }
+    const userCredential = await signInWithEmailAndPassword(
+      auth,
+      email,
+      password
+    )
 
-  if (response.redirected) {
-    window.location.assign(response.url);
-  }
-});
+    const idToken = await userCredential.user.getIdToken()
+    console.log(idToken)
+
+    const response = await fetch('/api/auth/login', {
+      method: 'GET',
+      headers: {
+        Authorization: `Bearer ${idToken}`
+      }
+    })
+
+    if (response.redirected) {
+      window.location.assign(response.url)
+    }
+  })
+}
