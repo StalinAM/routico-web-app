a89267071a06aa07e3c535a9b2003bd212b86d0a
Stalin Acurio <sta_vla411@hotmail.com>
2025-01-19 20:19:17 -0500

    add drivers store

--- src/store/useAuthStore.js
+++ src/store/useAuthStore.js
@@ -1,7 +1,22 @@
 import { create } from 'zustand'
+import { query, collection, getDocs, where } from 'firebase/firestore'
+import { db } from '../lib/firebase/client'
 
 export const useAuthStore = create((set) => ({
-  user: null,
-  setUser: (user) => set({ user }),
-  clearUser: () => set({ user: null })
+  drivers: [],
+  loading: false,
+  error: null,
+
+  fetchdrivers: async (uid) => {
+    set({ loading: true, error: null })
+    const driversList = []
+    const q = query(collection(db, 'drivers'), where('uidAdmin', '==', uid))
+    const querySnapshot = await getDocs(q)
+    querySnapshot.forEach((doc) => {
+      const driver = { ...doc.data() }
+      driver.docId = doc.id
+      driversList.push(driver)
+    })
+    set({ drivers: driversList, loading: false })
+  }
 }))
