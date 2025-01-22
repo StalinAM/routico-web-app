a8c2d7a518558e78252c7a1388c0fa50337d061e
Stalin Acurio <sta_vla411@hotmail.com>
2025-01-22 00:21:02 -0500

    feat: add functions to fetch routes and register drivers in Firebase service

--- src/utils/firebase/service.js
+++ src/utils/firebase/service.js
@@ -1,5 +1,6 @@
 import { query, collection, getDocs, where, addDoc } from 'firebase/firestore'
-import { db } from '../../lib/firebase/client'
+import { db, auth } from '../../lib/firebase/client'
+import { createUserWithEmailAndPassword, updateProfile } from 'firebase/auth'
 
 export const fetchDrivers = async (uid) => {
   const driversList = []
@@ -12,10 +13,43 @@ export const fetchDrivers = async (uid) => {
   return driversList
 }
 
+export const fetchRoutes = async (uid) => {
+  const routesList = []
+  const q = query(collection(db, 'routes'), where('uidAdmin', '==', uid))
+  const querySnapshot = await getDocs(q)
+  querySnapshot.forEach((doc) => {
+    const route = { ...doc.data(), docId: doc.id }
+    routesList.push(route)
+  })
+  return routesList
+}
+
 export const addDriver = async (driver) => {
   try {
     const driverRef = collection(db, 'drivers')
     await addDoc(driverRef, driver)
+  } catch (e) {
+    console.log(e)
+  }
+}
+
+export const addRoute = async (route) => {
+  try {
+    const routeRef = collection(db, 'routes')
+    await addDoc(routeRef, route)
+  } catch (e) {
+    console.log(e)
+  }
+}
+
+export const registerDriver = async (name, email, password) => {
+  try {
+    const driverCredentials = await createUserWithEmailAndPassword(
+      auth,
+      email,
+      password
+    )
+    await updateProfile(driverCredentials.user, { displayName: name })
   } catch (e) {
     console.log(e)
   }
