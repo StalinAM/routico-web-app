5d5501ba33bc19f8315fe1e9cf95f4cb8df1dd76
Stalin Acurio <sta_vla411@hotmail.com>
2025-01-26 01:31:02 -0500

    feat: refactor ListRoutes, Wrapper, and WrapperRoutes components to use icon components and simplify state management

--- src/components/Wrapper.jsx
+++ src/components/Wrapper.jsx
@@ -2,37 +2,21 @@ import { useState } from 'react'
 import { AddDriver } from './AddDriver'
 import { Search } from './Search'
 import { useAuthStore } from '../store/useAuthStore'
+import { AddDriverI } from './icons/Icons'
 
 export function Wrapper({ uid }) {
-  const { searchTerm, setSearchTerm } = useAuthStore()
-  const [isOpen, setIsOpen] = useState(false)
+  const { setOpenModal, searchTerm, setSearchTerm } = useAuthStore()
   return (
     <section className='flex justify-between gap-x-2 items-center'>
       <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
       <button
-        onClick={() => setIsOpen(true)}
-        className='flex items-center gap-x-1 bg-azur-600 text-azur-50/80 border rounded-xl py-2 px-4 hover:bg-azur-800 hover:text-azur-50'
+        onClick={() => setOpenModal(true)}
+        className='flex items-center gap-x-1 bg-azur-600 text-azur-50 border rounded-xl py-2 px-4 hover:bg-azur-800 '
       >
-        <svg
-          xmlns='http://www.w3.org/2000/svg'
-          width='24'
-          height='24'
-          viewBox='0 0 24 24'
-          fill='none'
-          stroke='currentColor'
-          strokeWidth='2'
-          strokeLinecap='round'
-          strokeLinejoin='round'
-        >
-          <path stroke='none' d='M0 0h24v24H0z' fill='none'></path>
-          <path d='M8 7a4 4 0 1 0 8 0a4 4 0 0 0 -8 0'></path>
-          <path d='M16 19h6'></path>
-          <path d='M19 16v6'></path>
-          <path d='M6 21v-2a4 4 0 0 1 4 -4h4'></path>
-        </svg>
+        <AddDriverI />
         Agregar
       </button>
-      <AddDriver uid={uid} isOpen={isOpen} setIsOpen={setIsOpen} />
+      <AddDriver uid={uid} />
     </section>
   )
 }
--- src/components/WrapperRoutes.jsx
+++ src/components/WrapperRoutes.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import { useRouteStore } from '../store/useRouteStore'
 import { AddRoute } from './AddRoute'
 import { Search } from './Search'
+import { AddRouteI } from './icons/Icons'
 
 export function WrapperRoutes({ uid }) {
   const { searchTerm, setSearchTerm } = useRouteStore()
@@ -13,23 +14,7 @@ export function WrapperRoutes({ uid }) {
         onClick={() => setIsOpen(true)}
         className='flex items-center gap-x-1 bg-azur-600 text-azur-50/80 border rounded-xl py-2 px-4 hover:bg-azur-800 hover:text-azur-50'
       >
-        <svg
-          xmlns='http://www.w3.org/2000/svg'
-          width='24'
-          height='24'
-          viewBox='0 0 24 24'
-          fill='none'
-          stroke='currentColor'
-          strokeWidth='2'
-          strokeLinecap='round'
-          strokeLinejoin='round'
-        >
-          <path stroke='none' d='M0 0h24v24H0z' fill='none'></path>
-          <path d='M8 7a4 4 0 1 0 8 0a4 4 0 0 0 -8 0'></path>
-          <path d='M16 19h6'></path>
-          <path d='M19 16v6'></path>
-          <path d='M6 21v-2a4 4 0 0 1 4 -4h4'></path>
-        </svg>
+        <AddRouteI />
         Agregar
       </button>
       <AddRoute uid={uid} isOpen={isOpen} setIsOpen={setIsOpen} />
