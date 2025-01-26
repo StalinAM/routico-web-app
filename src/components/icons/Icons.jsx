123abd8e3b070f1bb05a65c79587ac69abe3c8bb
Stalin Acurio <sta_vla411@hotmail.com>
2025-01-26 01:31:08 -0500

    feat: add new icon components for PingMap, AddRoute, and AddDriver

---
+++ src/components/icons/Icons.jsx
@@ -0,0 +1,61 @@
+export const PingMap = () => (
+  <svg
+    xmlns='http://www.w3.org/2000/svg'
+    width='24'
+    height='24'
+    viewBox='0 0 24 24'
+    fill='none'
+    stroke='currentColor'
+    strokeWidth='2'
+    strokeLinecap='round'
+    strokeLinejoin='round'
+  >
+    <path stroke='none' d='M0 0h24v24H0z' fill='none'></path>
+    <path d='M12 18.5l-3 -1.5l-6 3v-13l6 -3l6 3l6 -3v7'></path>
+    <path d='M9 4v13'></path>
+    <path d='M15 7v5'></path>
+    <path d='M21.121 20.121a3 3 0 1 0 -4.242 0c.418 .419 1.125 1.045 2.121 1.879c1.051 -.89 1.759 -1.516 2.121 -1.879z'></path>
+    <path d='M19 18v.01'></path>
+  </svg>
+)
+
+export const AddRouteI = () => (
+  <svg
+    xmlns='http://www.w3.org/2000/svg'
+    width='24'
+    height='24'
+    viewBox='0 0 24 24'
+    fill='none'
+    stroke='currentColor'
+    strokeWidth='2'
+    strokeLinecap='round'
+    strokeLinejoin='round'
+  >
+    <path stroke='none' d='M0 0h24v24H0z' fill='none'></path>
+    <path d='M12 18.5l-3 -1.5l-6 3v-13l6 -3l6 3l6 -3v8.5'></path>
+    <path d='M9 4v13'></path>
+    <path d='M15 7v8'></path>
+    <path d='M16 19h6'></path>
+    <path d='M19 16v6'></path>
+  </svg>
+)
+
+export const AddDriverI = () => (
+  <svg
+    xmlns='http://www.w3.org/2000/svg'
+    width='24'
+    height='24'
+    viewBox='0 0 24 24'
+    fill='none'
+    stroke='currentColor'
+    strokeWidth='2'
+    strokeLinecap='round'
+    strokeLinejoin='round'
+  >
+    <path stroke='none' d='M0 0h24v24H0z' fill='none'></path>
+    <path d='M8 7a4 4 0 1 0 8 0a4 4 0 0 0 -8 0'></path>
+    <path d='M16 19h6'></path>
+    <path d='M19 16v6'></path>
+    <path d='M6 21v-2a4 4 0 0 1 4 -4h4'></path>
+  </svg>
+)
