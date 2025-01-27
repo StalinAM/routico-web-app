fad0a5228a55944312cc01bb087b45cb4564a067
Stalin Acurio <sta_vla411@hotmail.com>
2025-01-27 00:46:47 -0500

    feat: update WrapperRoutes to manage modal state through useRouteStore

--- src/components/WrapperRoutes.jsx
+++ src/components/WrapperRoutes.jsx
@@ -1,23 +1,21 @@
-import { useState } from 'react'
 import { useRouteStore } from '../store/useRouteStore'
 import { AddRoute } from './AddRoute'
 import { Search } from './Search'
 import { AddRouteI } from './icons/Icons'
 
 export function WrapperRoutes({ uid }) {
-  const { searchTerm, setSearchTerm } = useRouteStore()
-  const [isOpen, setIsOpen] = useState(false)
+  const { searchTerm, setSearchTerm, setOpenModal } = useRouteStore()
   return (
     <section className='flex justify-between gap-x-2 items-center'>
       <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
       <button
-        onClick={() => setIsOpen(true)}
+        onClick={() => setOpenModal(true)}
         className='flex items-center gap-x-1 bg-azur-600 text-azur-50/80 border rounded-xl py-2 px-4 hover:bg-azur-800 hover:text-azur-50'
       >
         <AddRouteI />
         Agregar
       </button>
-      <AddRoute uid={uid} isOpen={isOpen} setIsOpen={setIsOpen} />
+      <AddRoute uid={uid} />
     </section>
   )
 }
