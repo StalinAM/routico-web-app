162ae69569864685326c918d34d4309203926982
Stalin Acurio <sta_vla411@hotmail.com>
2025-01-29 07:42:37 -0500

    feat: enhance HeaderDriver component with delivery status toggle and phone number display

--- src/components/HeaderDriver.jsx
+++ src/components/HeaderDriver.jsx
@@ -1,14 +1,37 @@
+import { useState } from 'react'
 import { useRouteStore } from '../store/useRouteStore'
 
 export function HeaderDriver() {
+  const [deliver, setDeliver] = useState(false)
   const { route } = useRouteStore()
-  console.log(route)
 
   return (
-    <header className='p-4 flex justify-between items-center'>
-      <h1 className='text-xl font-bold'>{route.routeName}</h1>
-      <button class='bg-azur-600 text-azur-50 border rounded-xl py-2 px-4 hover:bg-azur-800'>
-        Entegrado
+    <header className='flex justify-between items-center'>
+      <div>
+        <h1 className='text-xl font-bold'>{route.routeName}</h1>
+        <p className='flex gap-1 items-center'>
+          <svg
+            xmlns='http://www.w3.org/2000/svg'
+            width='20'
+            height='20'
+            viewBox='0 0 24 24'
+            fill='none'
+            stroke='currentColor'
+            strokeWidth='2'
+            strokeLinecap='round'
+            strokeLinejoin='round'
+          >
+            <path stroke='none' d='M0 0h24v24H0z' fill='none' />
+            <path d='M5 4h4l2 5l-2.5 1.5a11 11 0 0 0 5 5l1.5 -2.5l5 2v4a2 2 0 0 1 -2 2a16 16 0 0 1 -15 -15a2 2 0 0 1 2 -2' />
+          </svg>
+          <span>{route.phoneNumber}</span>
+        </p>
+      </div>
+      <button
+        onClick={() => setDeliver(!deliver)}
+        className='bg-azur-600 text-azur-50 border rounded-xl py-2 px-4 hover:bg-azur-800'
+      >
+        {deliver ? 'Entregado' : 'En camino'}
       </button>
     </header>
   )
