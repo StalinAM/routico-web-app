8e0f5e7f10ae81109fdd837e950af93dfee8b472
Stalin Acurio <sta_vla411@hotmail.com>
2025-01-29 00:32:41 -0500

    feat: remove unused import from ListRoutes component

--- src/components/ListRoutes.jsx
+++ src/components/ListRoutes.jsx
@@ -1,4 +1,4 @@
-import { use, useEffect, useState } from 'react'
+import { useEffect, useState } from 'react'
 import { useRouteStore } from '../store/useRouteStore'
 import { PingMap } from './icons/Icons'
 
