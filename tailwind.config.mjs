625504fd91003afab1e44822f5e00cba141cf613
Stalin Acurio <sta_vla411@hotmail.com>
2025-01-31 01:46:54 -0500

    feat: add scale-up and scale-down animations to Tailwind configuration

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -12,6 +12,20 @@ export default {
           800: '#3e388b'
         }
       }
+    },
+    keyframes: {
+      'scale-up-center': {
+        '0%': { transform: 'scale(0.5)' },
+        '100%': { transform: 'scale(1)' }
+      },
+      'scale-down-center': {
+        '0%': { transform: 'scale(1)' },
+        '100%': { transform: 'scale(0.5)' }
+      }
+    },
+    animation: {
+      'scale-up-center': 'scale-up-center 0.3s ease-out',
+      'scale-down-center': 'scale-down-center 0.3s ease-out'
     }
   },
   plugins: []
