cb575dc750388d8bde350a4abbb46a364f0a476d
Stalin Acurio <sta_vla411@hotmail.com>
2025-02-02 23:59:42 -0500

    feat: add new SVG icons for route management actions (Delete, Details, Check)

--- src/components/icons/Icons.jsx
+++ src/components/icons/Icons.jsx
@@ -99,3 +99,62 @@ export const MapPinExclamation = () => (
     <path d='M19 22v.01' />
   </svg>
 )
+
+export const DeleteRoute = () => (
+  <svg
+    xmlns='http://www.w3.org/2000/svg'
+    width='24'
+    height='24'
+    viewBox='0 0 24 24'
+    fill='none'
+    stroke='currentColor'
+    strokeWidth='2'
+    strokeLinecap='round'
+    strokeLinejoin='round'
+  >
+    <path stroke='none' d='M0 0h24v24H0z' fill='none' />
+    <path d='M6 19m-2 0a2 2 0 1 0 4 0a2 2 0 1 0 -4 0' />
+    <path d='M18 5m-2 0a2 2 0 1 0 4 0a2 2 0 1 0 -4 0' />
+    <path d='M12 19h4.5c.71 0 1.372 -.212 1.924 -.576m1.545 -2.459a3.5 3.5 0 0 0 -3.469 -3.965h-.499m-4 0h-3.501a3.5 3.5 0 0 1 -2.477 -5.972m2.477 -1.028h3.5' />
+    <path d='M3 3l18 18' />
+  </svg>
+)
+export const DetailsRoute = () => (
+  <svg
+    xmlns='http://www.w3.org/2000/svg'
+    width='24'
+    height='24'
+    viewBox='0 0 24 24'
+    fill='none'
+    stroke='currentColor'
+    strokeWidth='2'
+    strokeLinecap='round'
+    strokeLinejoin='round'
+  >
+    <path stroke='none' d='M0 0h24v24H0z' fill='none' />
+    <path d='M8 21h-2a3 3 0 0 1 -3 -3v-1h5.5' />
+    <path d='M17 8.5v-3.5a2 2 0 1 1 2 2h-2' />
+    <path d='M19 3h-11a3 3 0 0 0 -3 3v11' />
+    <path d='M9 7h4' />
+    <path d='M9 11h4' />
+    <path d='M18.42 12.61a2.1 2.1 0 0 1 2.97 2.97l-6.39 6.42h-3v-3z' />
+  </svg>
+)
+export const CheckRoute = () => (
+  <svg
+    xmlns='http://www.w3.org/2000/svg'
+    width='24'
+    height='24'
+    viewBox='0 0 24 24'
+    fill='none'
+    stroke='currentColor'
+    strokeWidth='2'
+    strokeLinecap='round'
+    strokeLinejoin='round'
+  >
+    <path stroke='none' d='M0 0h24v24H0z' fill='none' />
+    <path d='M9 21v-6a2 2 0 0 1 2 -2h2a2 2 0 0 1 2 2' />
+    <path d='M19 13.488v-1.488h2l-9 -9l-9 9h2v7a2 2 0 0 0 2 2h4.525' />
+    <path d='M15 19l2 2l4 -4' />
+  </svg>
+)
