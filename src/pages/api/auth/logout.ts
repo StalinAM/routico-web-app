a06099b90927614d9cae49c38543bd164d6f0b07
Stalin Acurio <sta_vla411@hotmail.com>
2025-02-10 15:15:30 -0500

    fix: enhance logout functionality with secure cookie deletion and redirection

--- src/pages/api/auth/logout.ts
+++ src/pages/api/auth/logout.ts
@@ -2,7 +2,18 @@ import type { APIRoute } from 'astro'
 
 export const GET: APIRoute = async ({ redirect, cookies }) => {
   cookies.delete('__session', {
-    path: '/'
+    path: '/',
+    secure: true, // Asegura que se elimina solo en HTTPS
+    httpOnly: true, // Evita que JavaScript pueda acceder a ella
+    sameSite: 'lax' // O 'none' si usas 'secure: true'
   })
-  return redirect('/signin')
+
+  return new Response(null, {
+    status: 302,
+    headers: {
+      'Set-Cookie':
+        '__session=; Path=/; Max-Age=0; Secure; HttpOnly; SameSite=Lax',
+      Location: '/signin'
+    }
+  })
 }
