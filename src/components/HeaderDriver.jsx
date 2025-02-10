6136b9e930fb38010293b7c1ec117f6788e2dd40
Stalin Acurio <sta_vla411@hotmail.com>
2025-02-10 12:24:28 -0500

    style: update SVG attributes in HeaderDriver component for consistency

--- src/components/HeaderDriver.jsx
+++ src/components/HeaderDriver.jsx
@@ -20,10 +20,9 @@ export function HeaderDriver() {
             viewBox='0 0 24 24'
             fill='none'
             stroke='currentColor'
-            stroke-width='2'
-            stroke-linecap='round'
-            stroke-linejoin='round'
-            class='icon icon-tabler icons-tabler-outline icon-tabler-arrow-narrow-left'
+            strokeWidth='2'
+            strokeLinecap='round'
+            strokeLinejoin='round'
           >
             <path stroke='none' d='M0 0h24v24H0z' fill='none' />
             <path d='M5 12l14 0' />
