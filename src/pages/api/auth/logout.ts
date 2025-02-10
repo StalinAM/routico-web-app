eea71164607c2d7159204c867fefc2a5a0d172f6
Stalin Acurio <sta_vla411@hotmail.com>
2025-02-10 16:59:39 -0500

    fix: enhance logout API to return a proper redirect response with cache control headers

--- src/pages/api/auth/logout.ts
+++ src/pages/api/auth/logout.ts
@@ -1,8 +1,16 @@
-import type { APIRoute } from 'astro'
+import { type APIRoute } from 'astro'
 
-export const GET: APIRoute = ({ redirect, cookies }) => {
-  cookies.delete('session', {
-    path: '/'
+export const GET: APIRoute = async ({ cookies, redirect }) => {
+  cookies.delete('session', { path: '/' })
+
+  return new Response(null, {
+    status: 302,
+    headers: {
+      Location: '/signin',
+      'Cache-Control':
+        'no-store, no-cache, must-revalidate, proxy-revalidate, max-age=0',
+      Pragma: 'no-cache',
+      Expires: '0'
+    }
   })
-  return redirect('/signin')
 }
