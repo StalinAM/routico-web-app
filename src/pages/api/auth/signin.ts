de5bd4957582173bd271e94a4e1f694af8328839
Stalin Acurio <sta_vla411@hotmail.com>
2025-02-10 18:16:44 -0500

    fix: replace login API with signin API and update session handling logic

---
+++ src/pages/api/auth/signin.ts
@@ -0,0 +1,37 @@
+import type { APIRoute } from 'astro'
+import { app } from '../../../lib/firebase/server'
+import { getAuth } from 'firebase-admin/auth'
+export const prerender = false
+export const GET: APIRoute = async ({ request, cookies, redirect }) => {
+  const auth = getAuth(app)
+
+  /* Obtener el token del header */
+  const idToken = request.headers.get('Authorization')?.split('Bearer ')[1]
+  if (!idToken) {
+    return new Response('No token found', { status: 401 })
+  }
+
+  /* Verify id token */
+  try {
+    await auth.verifyIdToken(idToken)
+  } catch (error) {
+    return new Response('Invalid token', { status: 401 })
+  }
+
+  /* Create and set session cookie */
+  const fiveDays = 60 * 60 * 24 * 5 * 1000
+  const sessionCookie = await auth.createSessionCookie(idToken, {
+    expiresIn: fiveDays
+  })
+
+  cookies.set('session', sessionCookie, {
+    path: '/'
+  })
+  const decodedCookie = await auth.verifySessionCookie(sessionCookie)
+  const user = await auth.getUser(decodedCookie.uid)
+
+  if (user.email?.match(/@routico.com/)) {
+    return redirect('/drivers')
+  }
+  return redirect('/admin')
+}
