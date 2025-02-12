b80b2e9d7876708ef37ed94d0d6fc122a2c5e5af
Stalin Acurio <sta_vla411@hotmail.com>
2025-02-11 22:43:05 -0500

    feat: render RoutineMachine component when both origin and destination are provided

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -69,9 +69,12 @@ const Map = ({ uid, id }) => {
         url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
       />
-      {/* {origin && waypoints.length > 0 && (
+      {origin && waypoints.length > 0 && (
         <RoutineMachine waypoints={[origin, ...waypoints, destination]} />
-      )} */}
+      )}
+      {origin && destination && (
+        <RoutineMachine waypoints={[origin, ...waypoints, destination]} />
+      )}
     </MapContainer>
   )
 }
